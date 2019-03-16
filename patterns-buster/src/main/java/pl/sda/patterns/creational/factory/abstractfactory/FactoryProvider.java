7b6a3d2b2603ac0dffb2de4778b2d01ed2089a10
marcinerbel <marcinerbel90@gmail.com>
2019-03-16 14:14:56 +0100

    SDA - Patterns Creational/Structural init
    #Workshops 1 (Singleton/Factory)
    Factory Provider
    
    🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈🌈

--- patterns-buster/src/main/java/pl/sda/patterns/creational/factory/abstractfactory/FactoryProvider.java
+++ patterns-buster/src/main/java/pl/sda/patterns/creational/factory/abstractfactory/FactoryProvider.java
@@ -1,20 +1,29 @@
 package pl.sda.patterns.creational.factory.abstractfactory;
 
+import static pl.sda.patterns.creational.factory.abstractfactory.DataType.*;
+
 public class FactoryProvider {
 
-    public AbstractFactory saveDate(DataType dataType,
-                                    String content,
-                                    String path) {
-        if (dataType == DataType.CSV) {
+    public AbstractFactory getFactory(DataType dataType,
+                                      String content,
+                                      String path) {
+        if (CSV == dataType) {
             return new CsvFactory();
-        } else if (dataType == DataType.PDF) {
+        } else if (PDF == dataType) {
             return new PdfFactory();
-        } else if (dataType == DataType.JSON) {
+        } else if (JSON == dataType) {
             return new JsonFactory();
         } else {
             System.out.println("ups");
-            return null;
+            return null; //TODO exception
         }
+    }
+
+    public void saveData(DataType dataType,
+                         String content,
+                         String path) {
+        AbstractFactory factory = getFactory(dataType, content, path);
+        factory.save(content, path);
     }
 
 }
